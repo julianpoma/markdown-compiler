ef8f0f8422fc179d6bcfcc15ffcce90e9a059333
Julian Poma <poma.nob@gmail.com>
2019-12-31 12:18:03 -0300

    Read file and parse

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,7 @@
-/**
- * https://jesselawson.org/rust/getting-started-with-rust-by-building-a-tiny-markdown-compiler/
- */
-use std::fs::File;
-use std::path::Path;
 use std::error::Error;
+use std::fs::File;
+use std::io::{BufRead, BufReader};
+use std::path::Path;
 
 fn get_title() -> String {
     let mut title = String::from(env!("CARGO_PKG_NAME"));
@@ -24,22 +22,64 @@ fn print_separation_bars(separator: &str, longitude: usize) {
     println!("{}", separator.repeat(longitude));
 }
 
-fn parse_markdown(file: &str) {
+fn parse_markdown(file_name: &str) {
     print_title();
-    println!("[INFO] Parsing {} ...", file);
+    println!("[INFO] Parsing {} ...", file_name);
 
-    let file_path = Path::new(file);
+    let file_path = Path::new(file_name);
 
-    let file_buffer = match File::open(file_path) {
+    let file = match File::open(file_path) {
         Ok(f) => f,
         Err(err) => {
-            println!("[ERROR] Something went wrong reading the file: {}", file);
+            println!(
+                "[ERROR] Something went wrong reading the file: {}",
+                file_name
+            );
             println!("[INFO] Maybe the path is wrong");
             panic!("{}", err.description());
         }
     };
 
-    println!("{:?}", file_buffer);
+    let mut _h1_tag: bool = false;
+    let mut _p_tag: bool = false;
+
+    let mut tokens: Vec<String> = Vec::new();
+
+    let file_buffer = BufReader::new(file);
+
+    for line in file_buffer.lines() {
+        let line_content = line.unwrap().to_string();
+        let mut first_char: Vec<char> = line_content.chars().take(1).collect();
+
+        match first_char.pop() {
+            Some('#') => {
+                _h1_tag = true;
+                _p_tag = false;
+            }
+            _ => {
+                _h1_tag = false;
+                _p_tag = true;
+            }
+        }
+
+        let mut output = String::new();
+
+        if _h1_tag {
+            output.push_str("<h1>");
+            output.push_str(&line_content[2..]);
+            output.push_str("</h1>");
+        }
+
+        if _p_tag {
+            output.push_str("<p>");
+            output.push_str(&line_content[..]);
+            output.push_str("</p>");
+        }
+
+        tokens.push(output);
+    }
+
+    println!("{:?}", tokens);
 }
 
 fn usage() {
