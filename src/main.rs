16629c333dab9ef77f4d7922c7b63b080ae4afb5
Julian Poma <poma.nob@gmail.com>
2019-12-31 10:47:11 -0300

    Banner

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
+/**
+ * https://jesselawson.org/rust/getting-started-with-rust-by-building-a-tiny-markdown-compiler/
+ */
+fn usage() {
+    let description = env!("CARGO_PKG_DESCRIPTION");
+    println!("{}", description);
+    println!("Version: {}", env!("CARGO_PKG_VERSION"));
+    println!("{}", "=".repeat(description.len()));
+}
+
 fn main() {
-    println!("Hello, world!");
+    usage();
 }
